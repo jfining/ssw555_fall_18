c3c2b7a5037ff4adb758086d68edc90f7362a533
jverzosa <jverzosa@me.com>
2018-09-21 21:23:53 -0400

    User Story 2 & 3. Still a work in progress- will need team to review.

--- src/main/java/com/rasodu/gedcom/Validation/FamilyValidator.java
+++ src/main/java/com/rasodu/gedcom/Validation/FamilyValidator.java
@@ -1,15 +1,20 @@
 package com.rasodu.gedcom.Validation;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import com.rasodu.gedcom.Utils.GedLogger;
 import com.rasodu.gedcom.core.Family;
 import com.rasodu.gedcom.core.Individual;
 
 public class FamilyValidator implements IValidator {
 
+	GedLogger log = new GedLogger();
+
 	List<Family> familyList;
 	List<Individual> individualList;
-	
+
 	public FamilyValidator(List<Family> familyList, List<Individual> individualList) {
 		super();
 		this.familyList = familyList;
@@ -32,10 +37,46 @@ public class FamilyValidator implements IValidator {
 		this.individualList = individualList;
 	}
 
+	// US02
+
+	public boolean birthBeforeMarriage() {
+		String userStory = "US02";
+		boolean valid = true;
+
+		for (Family fam : familyList) {
+			for (Individual ind : individualList) {
+				if (fam.Married != null) {
+					if (fam.HusbandId.equals(ind.Id)) {
+						if (fam.Married.before(ind.Birthday)) {
+							log.error(userStory, null, fam, "Marriage is before Husband birthday.");
+							valid = false;
+						}
+						if (fam.WifeId.equals(ind.Id)) {
+							if (fam.Married.before(ind.Birthday)) {
+								log.error(userStory, null, fam, "Marriage is before Wife birthday.");
+								valid = false;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return valid;
+
+	}
+	
+	
+
 	@Override
 	public boolean validate() {
-		// TODO Auto-generated method stub
-		return true;
+		boolean allTestsValid = true;
+
+		if (!birthBeforeMarriage()) {
+			allTestsValid = false;
+		}
+
+		return allTestsValid;
 	}
 
 }
--- src/main/java/com/rasodu/gedcom/Validation/IndividualValidator.java
+++ src/main/java/com/rasodu/gedcom/Validation/IndividualValidator.java
@@ -2,14 +2,17 @@ package com.rasodu.gedcom.Validation;
 
 import java.util.List;
 
+import com.rasodu.gedcom.Utils.GedLogger;
 import com.rasodu.gedcom.core.Family;
 import com.rasodu.gedcom.core.Individual;
 
 public class IndividualValidator implements IValidator {
 
+	GedLogger log = new GedLogger();
+
 	List<Family> familyList;
 	List<Individual> individualList;
-	
+
 	public IndividualValidator(List<Family> familyList, List<Individual> individualList) {
 		super();
 		this.familyList = familyList;
@@ -32,10 +35,34 @@ public class IndividualValidator implements IValidator {
 		this.individualList = individualList;
 	}
 
+	// US03
+
+	public boolean birthBeforeDeath() {
+		String userStory = "US03";
+		boolean valid = true;
+
+		for (Individual ind : individualList) {
+			if (ind.Id != null) {
+				if (ind.Birthday.after(ind.Death)) {
+					log.error(userStory, ind, null, "Death date is before birthday");
+					valid = false;
+				}
+			}
+		}
+
+		return valid;
+
+	}
+
 	@Override
 	public boolean validate() {
-		// TODO Auto-generated method stub
-		return true;
+		boolean allTestsValid = true;
+
+		if (!birthBeforeDeath()) {
+			allTestsValid = false;
+		}
+
+		return allTestsValid;
 	}
 
 }
