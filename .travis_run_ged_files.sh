30b42890036ce534cc805c02474040b9350eb56f
Abhijit Amin <abhijitramin@gmail.com>
2018-09-30 01:50:01 -0400

    Add acceptance test output.

--- .travis_run_ged_files.sh
+++ .travis_run_ged_files.sh
@@ -7,4 +7,5 @@ cd target
 java -jar gedcom-disk-1.0-SNAPSHOT.jar ../Deliverables/Project3/proj03test.ged
 java -jar gedcom-disk-1.0-SNAPSHOT.jar ../Deliverables/Project4/proj4test.ged
 java -jar gedcom-disk-1.0-SNAPSHOT.jar ../Deliverables/UserStories/test_us06.ged
+java -jar gedcom-disk-1.0-SNAPSHOT.jar ..Deliverables/UserStories/test_us04.ged
 java -jar gedcom-disk-1.0-SNAPSHOT.jar ..Deliverables/UserStories/test_us08.ged
